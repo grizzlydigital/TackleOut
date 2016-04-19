5336d52e8e26d2e2535175186ccad59f5c64a902
Mike Adams <mike.adams@grizzlydigital.uk>
2016-04-19 09:49:31 +0100

    Added MVC, ReactJS and completed first controller test

---
+++ TackleOut.Tests/Controllers.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Text
+Imports System.Web.Mvc
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports TackleOut.Controllers
+
+Namespace TackleOutTests.Controllers
+
+    <TestClass()>
+    Public Class HomeControllerTest
+
+        <TestMethod()>
+        Public Sub TestIndexView()
+            Dim controller = New HomeController()
+            Dim result As ViewResult = controller.Index()
+            Assert.AreEqual("Index", result.ViewName)
+        End Sub
+
+    End Class
+
+End Namespace
